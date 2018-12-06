1d74fda7d745a4f9d28694397019f296900c3f7f
huyewei <huyewei@autohome.com.cn>
2018-12-06 09:59:44 +0800

    复习随机数的生成

--- js/getRandom.js
+++ js/getRandom.js
@@ -33,3 +33,36 @@ function getRandomIntInclusive(min,max){
     return Math.floor(Math.random()*(max-min+1))+min;
 }
 console.log(getRandomIntInclusive(2,5));
+
+
+
+//[0,1)
+function getRandom1(){
+    return Math.random();
+}
+console.log('获取[0,1)之间的随机数:'+getRandom1());
+
+// (2,5) 两个数之间的随机数
+function getRadomBetTwo(min,max){
+    return Math.random()*(max - min) + min;
+}
+console.log('获取两个数字之间的随机数正数'+getRadomBetTwo(2,5));
+console.log('获取两个数字之间的随机数负数'+getRadomBetTwo(-5,-2));
+
+//获得两个数字之间的随机整数
+function getRandomInt1(min,max){
+     min = Math.ceil(min);
+     max = Math.floor(max);
+     return Math.floor(Math.random()*(max-min)) + min;
+
+}
+console.log("获取两个数之间的随机整数"+ getRandomInt1(2,6));
+
+//获取两个数字之间的随机整数，包括这两个数字
+function getRandomIntInclusive1(min,max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random()*(max-min+1)) + min;
+
+}
+console.log("获取两个数字之间的随机整数，包含这两个数字"+ getRandomIntInclusive1(3,7));
